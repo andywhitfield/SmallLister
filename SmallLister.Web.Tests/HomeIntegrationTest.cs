f3fe8aee6e963f6d27b87ce3fbfb4aa55dc0d467
Andy Whitfield <andy@andywhitfield.me.uk>
2021-01-23 12:33:11 +0000

    Moved class to new file

--- SmallLister.Web.Tests/HomeIntegrationTest.cs
+++ SmallLister.Web.Tests/HomeIntegrationTest.cs
@@ -1,18 +1,8 @@
 using System;
 using System.Net;
-using System.Net.Http;
-using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using FluentAssertions;
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc.Testing;
-using Microsoft.AspNetCore.TestHost;
-using Microsoft.Data.Sqlite;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.DependencyInjection.Extensions;
-using Microsoft.Extensions.Hosting;
 using SmallLister.Data;
 using SmallLister.Model;
 using Xunit;
@@ -58,49 +48,12 @@ namespace SmallLister.Web.Tests
                 .And.Contain("Repeats every day")
                 .And.Contain("Due today");
         }
+        
 
         public Task DisposeAsync()
         {
             _factory.Dispose();
             return Task.CompletedTask;
-        }
-    }
-
-    public class IntegrationTestWebApplicationFactory : WebApplicationFactory<Startup>
-    {
-        private readonly SqliteConnection _connection;
-        private readonly DbContextOptions<SqliteDataContext> _options;
-
-        public IntegrationTestWebApplicationFactory()
-        {
-            _connection = new SqliteConnection("DataSource=:memory:");
-            _connection.Open();
-            _options = new DbContextOptionsBuilder<SqliteDataContext>().UseSqlite(_connection).Options;
-        }
-
-        protected override IHostBuilder CreateHostBuilder() => Host
-            .CreateDefaultBuilder()
-            .ConfigureWebHostDefaults(x => x.UseStartup<Startup>().UseTestServer().ConfigureTestServices(services =>
-            {
-                services.Replace(ServiceDescriptor.Scoped<SqliteDataContext>(_ => new SqliteDataContext(_options)));
-                services
-                    .AddAuthentication("Test")
-                    .AddScheme<AuthenticationSchemeOptions, TestStubAuthHandler>("Test", null);
-            }));
-
-        public HttpClient CreateAuthenticatedClient()
-        {
-            var client = CreateClient(new WebApplicationFactoryClientOptions { AllowAutoRedirect = false });
-            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Test");
-            return client;
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            base.Dispose(disposing);
-
-            if (disposing)
-                _connection.Dispose();
         }
     }
 }
