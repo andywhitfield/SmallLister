faafede0876905f958363b0998617769a3ef1c2d
Andy Whitfield <andy@andywhitfield.me.uk>
2023-12-20 10:50:07 +0000

    One more nullable fix to avoid BadRequest

--- SmallLister.Web/Controllers/HomeController.cs
+++ SmallLister.Web/Controllers/HomeController.cs
@@ -29,7 +29,7 @@ public class HomeController(IMediator mediator) : Controller
     public IActionResult Error() => View(new ErrorViewModel(HttpContext));
 
     [HttpGet("~/signin")]
-    public IActionResult Signin([FromQuery] string returnUrl) => View("Login", new LoginViewModel(HttpContext, returnUrl));
+    public IActionResult Signin([FromQuery] string? returnUrl) => View("Login", new LoginViewModel(HttpContext, returnUrl));
 
     [HttpPost("~/signin")]
     [ValidateAntiForgeryToken]
